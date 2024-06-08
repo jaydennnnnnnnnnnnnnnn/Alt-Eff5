283accba1f4e3143ac709e0c1ffa76281ce8c1fb
zodexus <164297282+zodexus@users.noreply.github.com>
2024-06-08 16:59:29 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { useFonts } from 'expo-font';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
+
+const localImage = require('./logo.jpeg');
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+    'Inter': require('./assets/fonts/Inter.ttc'),
+  });
+
+  return (
+    <View style={styles.container}>
+        <Text style={[styles.header, styles.fontSizeLarge]}>Welcome to EcoShopper</Text>
+        <Image
+          // source={{{ uri: 'https://files.libero.pe/Libero/2020/06/07/kfc-1591562357.jpg' }} }
+          source={ localImage }
+          style={ styles.image }
+        />
+        <Text style={{ color: 'purple' }}>User</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
+  },
+  header: {
+    justifyContent: 'center',
+  },
+  fontSizeLarge: {
+    fontSize: 60,
+  },
+  image: { 
+    width: 100,
+    height: 100,
+  }
+});
