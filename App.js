c9a34afcdf41379dfa0c169895f779761651cc35
jaydennnnnnnnnnnnnnnn <ljkjayden@gmail.com>
2024-06-08 23:38:49 +0800

    added screens, follow template in boilerplate and add import <put screen name> from <directory> (see app.js for example)

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="Login">
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
             </Stack.Navigator>
